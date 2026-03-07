25b1db3cddd7fb7475ddcb51b7cb48261da99be4
Oleksandr Mykhailov <folmy001@edu.xamk.fi>
2026-03-07 16:22:38 +0200

    Implemented Jumping and fixed infinite jumps

--- Grape Jam/Assets/PlayerMovement.cs
+++ Grape Jam/Assets/PlayerMovement.cs
@@ -4,16 +4,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rigidBody;
-    public float movementSpeed = 5f;
 
+    [Header("Movement")]
+    public float movementSpeed = 5f;
     float horizontalMovement;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [Header("Jumping")]
+    public float jumpPower = 10f;
 
-
-    }
+    [Header("GroundCheck")]
+    public Transform groundCheck;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
@@ -25,5 +27,35 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded())
+        {
+            if (context.performed)
+            {
+                rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpPower);
+            }
+            else if (context.canceled)
+            {
+                rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpPower * 0.5f);
+            }
+        }
+    }
+
+    private bool isGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, groundLayer))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(groundCheck.position, groundCheckSize);
     }
 }
